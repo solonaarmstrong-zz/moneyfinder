73d434e861066384572818220af6f13846156d90
Solona Armstrong <solona.armstrong@gmail.com>
2017-09-11 09:53:07 -0400

    Data loads from json file and applied some styles

--- demo-app/src/app/app.component.ts
+++ demo-app/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component, Input } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [
+		'./app.component.css']
 })
 export class AppComponent {
-  title = 'UI Web Developer Programming Test';
-  author = 'Solona'
+	title = 'UI Web Developer Programming Test';
+	author = 'Solona';
+	data;
+	constructor(private http:Http) {
+		this.http.get('assets/list-data.json')
+		.subscribe(res => this.data = res.json());
+	}
 }
